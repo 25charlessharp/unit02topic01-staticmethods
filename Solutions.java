47407455660240eb591b33b1769f30528964013e
Charles Sharp <25charlessharp@usd266.com>
2023-01-31 12:04:17 -0600

    max3 done

--- Solutions.java
+++ Solutions.java
@@ -1,5 +1,30 @@
+import javax.sql.rowset.spi.SyncResolver;
+
 public class Solutions {
+    public static int max3(int a, int b, int c) {
+        if( a > b && a > c){
+            return a;
+        }
+        else if( b > a && b > c){
+            return b;
+        }
+        else{
+            return c;
+        }
+    }
+    public static double max3(double a, double b, double c) {
+        if( a > b && a > c){
+            return a;
+        }
+        else if( b > a && b > c){
+            return b;
+        }
+        else{
+            return c;
+        }
+    }
+
     public static void main(String[] args) {
-        
+        System.out.println(max3(1,2,0.000001));
     }
 }
